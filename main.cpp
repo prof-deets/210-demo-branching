8cdd7b4a0cde232b6af353d532dbbd48913cc44f
prof-deets <mark.dietrich@gmail.com>
2024-11-01 17:11:22 -0400

    added function1 functionality

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 using namespace std;
 
+void first_function(string);
+
 int main() {
-    cout << "COMSC 210\n";
+    cout << "COMSC-210\n";
 
     // section 1
+    string str = "COMSC-210";
+    first_function(str);
 
     // section 2
 
     // section 3
 
     return 0;
+}
+
+void first_function(string s) {
+    cout << s << endl;
 }
